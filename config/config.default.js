ee1cc38eba5dbb5cf052a996d88201f8d33f7f4d
leowang <leowangj108@gmail.com>
2020-08-16 02:59:27 +0800

    ✨ add redis

--- config/config.default.js
+++ config/config.default.js
@@ -1,39 +1,56 @@
 /* eslint valid-jsdoc: "off" */
 
-'use strict';
+"use strict"
 
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
-module.exports = appInfo => {
+module.exports = (appInfo) => {
   /**
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {};
+  const config = (exports = {})
 
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1597369312667_9666';
+  config.keys = appInfo.name + "_1597369312667_9666"
 
   // add your middleware config here
-  config.middleware = [];
+  config.middleware = []
 
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
-  };
+  }
 
   config.sequelize = {
-    dialect: 'mysql',
-    host: '127.0.0.1',
+    dialect: "mysql",
+    host: "127.0.0.1",
     port: 3307,
-    database: 'blog',
-    username: 'root',
-    password: '123456',
-  };
+    database: "blog",
+    username: "root",
+    password: "123456",
+  }
+
+  config.redis = {
+    client: {
+      port: 6379, // Redis port
+      host: "127.0.0.1", // Redis host
+      password: "",
+      db: 0,
+    },
+    agent: true,
+  }
+
+  config.sessionRedis = {
+    key: "EGG_BLOG_SESSION",
+    maxAge: 1 * 3600 * 1000,
+    httpOnly: true,
+    encrypt: false,
+  }
 
   return {
     ...config,
     ...userConfig,
-  };
-};
+  }
+}
--- config/plugin.js
+++ config/plugin.js
@@ -1,6 +1,15 @@
-
 // eslint-disable-next-line strict
 exports.mysql = {
   enable: true,
-  package: 'egg-sequelize',
-};
+  package: "egg-sequelize",
+}
+
+exports.redis = {
+  enable: true,
+  package: "egg-redis",
+}
+
+exports.sessionRedis = {
+  enable: true,
+  package: "egg-session-redis",
+}
