b27717f455907df36fd6e5276363a82600b96d58
leowang <leowang@jetfuel.com.tw>
2020-08-19 11:37:45 +0800

    ✨ 新增users seeds

--- seeders/20200818100511-users.js
+++ seeders/20200818100511-users.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const bcrypt = require('bcrypt');
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     /**
@@ -11,6 +11,38 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+
+    return await queryInterface.bulkInsert('users', [{
+      user_name: 'root',
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      nickname: 'root',
+      gender: 1,
+      picture: null,
+      city: '台中',
+      auth: 3,
+      updated_at: new Date(),
+      created_at: new Date(),
+    }, {
+      user_name: 'user1',
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      nickname: 'user1',
+      gender: 2,
+      picture: null,
+      city: '台中',
+      auth: 3,
+      updated_at: new Date(),
+      created_at: new Date(),
+    }, {
+      user_name: 'user2',
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      nickname: 'user2',
+      gender: 3,
+      picture: null,
+      city: '台中',
+      auth: 3,
+      created_at: new Date(),
+      updated_at: new Date(),
+    }]);
   },
 
   down: async (queryInterface, Sequelize) => {
@@ -20,5 +52,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+    return await queryInterface.bulkDelete('users', null, {});
+  },
 };
